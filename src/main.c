76fa0199338e74229cd3e43c1db5b769de1e1114
struc2ture <struc0ture@gmail.com>
2025-05-13 16:52:01 -0700

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main() {
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+    if (!window) return -1;
+
+    glfwMakeContextCurrent(window);
+
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
