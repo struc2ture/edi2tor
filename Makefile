fe8d64f10e23f8580347ad247c4f519195b179f7
struc2ture <struc0ture@gmail.com>
2025-05-13 18:26:45 -0700

    Use stb_easy_font to render text

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 bin/editor: src/main.c
-	clang src/main.c -lglfw -framework OpenGL -I/opt/homebrew/include -L/opt/homebrew/lib -DGL_SILENCE_DEPRECATION -Wall -Wextra -Werror -o bin/editor
+	clang src/main.c -lglfw -framework OpenGL -I/opt/homebrew/include -isystemthird_party -L/opt/homebrew/lib -DGL_SILENCE_DEPRECATION -Wall -Wextra -Werror -o bin/editor
 
 run: bin/editor
 	bin/editor
--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
+
 #include <OpenGL/gl3.h>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
+#include <stb_easy_font.h>
+
 
 const char *vs_src =
 "#version 410 core\n"
 "layout (location = 0) in vec2 aPos;\n"
 "void main() {\n"
-"    gl_Position = vec4(aPos, 0.0, 1.0);\n"
+"    vec2 zeroToOne = aPos / vec2(800, 600);\n"
+"    vec2 zeroToTwo = zeroToOne * 2.0;\n"
+"    vec2 clipSpace = zeroToTwo - 1.0;\n"
+"    gl_Position = vec4(clipSpace.x, -clipSpace.y, 0.0, 1.0);\n"
 "}";
 
 const char *fs_src =
@@ -30,7 +36,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
     if (!window) return -1;
 
     glfwMakeContextCurrent(window);
@@ -40,6 +46,18 @@ int main() {
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vs_src, 0);
     glCompileShader(vs);
+    // {
+    //     GLuint shader = vs;
+    //     GLint success = 0;
+    //     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    //     if (!success) {
+    //         char log[512];
+    //         glGetShaderInfoLog(shader, sizeof(log), NULL, log);
+    //         fprintf(stderr, "Shader compile error:\n%s\nSource:\n", log);
+    //         fprintf(stderr, "%s\n", vs_src);
+
+    //     }
+    // }
 
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fs, 1, &fs_src, 0);
@@ -52,12 +70,39 @@ int main() {
     glDeleteShader(vs);
     glDeleteShader(fs);
 
+    char text[] = "HELLO WORLD!!!";
+    char buffer[99999];
+    float triangle_buffer[6 * 2 * 9999]; // 6 vertices (x, y) per quad
+    int quad_count = stb_easy_font_print(10, 10, text, NULL, buffer, sizeof(buffer));
+    int vert_count = quad_count * 6;
+
+    // stb_easy_font outputs quads: 4 vertices per glyph box
+    {
+        float *src = (float *)buffer;
+
+        for (int i = 0; i < quad_count; ++i) {
+            float *q = &src[i * 4 * 4];
+
+            float *dst = &triangle_buffer[i * 6 * 2];
+
+            // Triangle 1
+            dst[0] = q[0]; dst[1] = q[1];
+            dst[2] = q[4]; dst[3] = q[5];
+            dst[4] = q[8]; dst[5] = q[9];
+
+            // Triangle 2
+            dst[6] = q[0];  dst[7] = q[1];
+            dst[8] = q[8];  dst[9] = q[9];
+            dst[10] = q[12]; dst[11] = q[13];
+        }
+    }
+
     GLuint vao, vbo;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vert_count * 2 * sizeof(float), triangle_buffer, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
@@ -66,7 +111,7 @@ int main() {
 
         glUseProgram(prog);
         glBindVertexArray(vao);
-        glDrawArrays((GL_TRIANGLES), 0, 3);
+        glDrawArrays((GL_TRIANGLES), 0, vert_count);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
