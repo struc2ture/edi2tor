a8db4070bf86bad69d0af02246aff322c6d4e4f7
struc2ture <struc0ture@gmail.com>
2025-05-13 17:36:17 -0700

    GL triangle

--- src/main.c
+++ src/main.c
@@ -1,5 +1,26 @@
+#include <OpenGL/gl3.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+
+const char *vs_src =
+"#version 410 core\n"
+"layout (location = 0) in vec2 aPos;\n"
+"void main() {\n"
+"    gl_Position = vec4(aPos, 0.0, 1.0);\n"
+"}";
+
+const char *fs_src =
+"#version 410 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"    FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+"}";
+
+const float vertices[] = {
+    -0.5f, -0.5f,
+     0.5f, -0.5f,
+     0.0f,  0.5f
+};
 
 int main() {
     if (!glfwInit()) return -1;
@@ -16,8 +37,37 @@ int main() {
 
     printf("OpenGL version: %s\n", glGetString(GL_VERSION));
 
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vs_src, 0);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fs_src, 0);
+    glCompileShader(fs);
+
+    GLuint prog = glCreateProgram();
+    glAttachShader(prog, vs);
+    glAttachShader(prog, fs);
+    glLinkProgram(prog);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(prog);
+        glBindVertexArray(vao);
+        glDrawArrays((GL_TRIANGLES), 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
