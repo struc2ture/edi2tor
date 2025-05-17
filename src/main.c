5d4f2ccec137e3e70c7d14bae824554bc588be22
struc2ture <struc0ture@gmail.com>
2025-05-16 20:44:11 -0700

    Render line colors

--- src/main.c
+++ src/main.c
@@ -10,18 +10,22 @@
 const char *vs_src =
 "#version 410 core\n"
 "layout (location = 0) in vec2 aPos;\n"
+"layout (location = 1) in vec4 aColor;\n"
+"out vec4 Color;\n"
 "void main() {\n"
 "    vec2 zeroToOne = aPos / vec2(800, 600);\n"
 "    vec2 zeroToTwo = zeroToOne * 2.0;\n"
 "    vec2 clipSpace = zeroToTwo - 1.0;\n"
 "    gl_Position = vec4(clipSpace.x, -clipSpace.y, 0.0, 1.0);\n"
+"    Color = aColor;\n"
 "}";
 
 const char *fs_src =
 "#version 410 core\n"
 "out vec4 FragColor;\n"
+"in vec4 Color;\n"
 "void main() {\n"
-"    FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+"    FragColor = Color;\n"
 "}";
 
 typedef struct {
@@ -31,7 +35,12 @@ typedef struct {
 } Text_Buffer;
 
 typedef struct {
-    float verts[VERT_MAX * 2];
+    float x, y;
+    unsigned char r, g, b, a;
+} Vert;
+
+typedef struct {
+    Vert verts[VERT_MAX];
     int vert_count;
 } Vert_Buffer;
 
@@ -43,7 +52,8 @@ void char_callback(GLFWwindow *window, unsigned int codepoint);
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 Text_Buffer read_file(const char *path);
-void render_line(int x, int y, char *line, Vert_Buffer *out_vert_buf);
+void render_string(int x, int y, char *line, unsigned char color[4], Vert_Buffer *out_vert_buf);
+void draw_string(int x, int y, char *string, unsigned char color[4]);
 void append_char_to_line(Text_Buffer *text_buffer, int cursor_line, char c);
 void new_line(Text_Buffer *text_buffer, int cursor_line);
 void remove_line(Text_Buffer *text_buffer, int cursor_line);
@@ -69,23 +79,22 @@ int main() {
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vs_src, 0);
     glCompileShader(vs);
-    // {
-    //     GLuint shader = vs;
-    //     GLint success = 0;
-    //     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    //     if (!success) {
-    //         char log[512];
-    //         glGetShaderInfoLog(shader, sizeof(log), NULL, log);
-    //         fprintf(stderr, "Shader compile error:\n%s\nSource:\n", log);
-    //         fprintf(stderr, "%s\n", vs_src);
-
-    //     }
-    // }
 
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fs, 1, &fs_src, 0);
     glCompileShader(fs);
+    {
+        GLuint shader = vs;
+        GLint success = 0;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            char log[512];
+            glGetShaderInfoLog(shader, sizeof(log), NULL, log);
+            fprintf(stderr, "Shader compile error:\n%s\nSource:\n", log);
+            fprintf(stderr, "%s\n", vs_src);
 
+        }
+    }
     GLuint prog = glCreateProgram();
     glAttachShader(prog, vs);
     glAttachShader(prog, fs);
@@ -99,8 +108,12 @@ int main() {
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER,  VERT_MAX * 2 * sizeof(float), NULL, GL_DYNAMIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vert), (void *)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vert), (void *)offsetof(Vert, r));
+    glEnableVertexAttribArray(1);
 
     g_text_buffer = read_file("res/mock.txt");
 
@@ -110,15 +123,29 @@ int main() {
         glUseProgram(prog);
         glBindVertexArray(vao);
 
+        int x_offset = 10;
         int y_offset = 10;
         int line_height = 20;
 
         for (int line_i = g_first_line; line_i < g_text_buffer.line_count; line_i++) {
-            Vert_Buffer vert_buf = {0};
-            render_line(10, y_offset, g_text_buffer.lines[line_i], &vert_buf);
-            glBufferSubData(GL_ARRAY_BUFFER, 0, vert_buf.vert_count * 2 * sizeof(float), vert_buf.verts);
-            glDrawArrays((GL_TRIANGLES), 0, vert_buf.vert_count);
+            unsigned char color[] = { 240, 240, 240, 240 };
+            if (line_i == g_edit_line) {
+                color[0] = 50;
+                color[1] = 50;
+                color[2] = 255;
+                color[0] = 255;
+            }
 
+            char line_i_str_buf[256];
+            snprintf(line_i_str_buf, sizeof(line_i_str_buf), "%3d: ", line_i);
+
+            draw_string(x_offset, y_offset, line_i_str_buf, color);
+
+            x_offset += stb_easy_font_width(line_i_str_buf);
+
+            draw_string(x_offset, y_offset, g_text_buffer.lines[line_i], color);
+
+            x_offset = 10;
             y_offset += line_height;
         }
 
@@ -185,7 +212,8 @@ Text_Buffer read_file(const char *path) {
     return result;
 }
 
-void render_line(int x, int y, char *line, Vert_Buffer *out_vert_buf) {
+void render_string(int x, int y, char *line, unsigned char color[4], Vert_Buffer *out_vert_buf) {
+    (void)color;
     char quad_buf[99999];
     int quad_count = stb_easy_font_print(x, y, line, NULL, quad_buf, sizeof(quad_buf));
     out_vert_buf->vert_count = quad_count * 6;
@@ -197,19 +225,34 @@ void render_line(int x, int y, char *line, Vert_Buffer *out_vert_buf) {
         for (int i = 0; i < quad_count; ++i) {
             float *q = &src[i * 4 * 4];
 
-            float *dst = &out_vert_buf->verts[i * 6 * 2];
+            Vert *dst = &out_vert_buf->verts[i * 6];
 
             // Triangle 1
-            dst[0] = q[0]; dst[1] = q[1];
-            dst[2] = q[4]; dst[3] = q[5];
-            dst[4] = q[8]; dst[5] = q[9];
+            dst[0].x = q[0];  dst[0].y = q[1];
+            dst[1].x = q[4];  dst[1].y = q[5];
+            dst[2].x = q[8];  dst[2].y = q[9];
 
             // Triangle 2
-            dst[6] = q[0];  dst[7] = q[1];
-            dst[8] = q[8];  dst[9] = q[9];
-            dst[10] = q[12]; dst[11] = q[13];
+            dst[3].x = q[0];  dst[3].y = q[1];
+            dst[4].x = q[8];  dst[4].y = q[9];
+            dst[5].x = q[12]; dst[5].y = q[13];
+
+            for (int i = 0; i < 6; i++) {
+                dst[i].r = color[0];
+                dst[i].g = color[1];
+                dst[i].b = color[2];
+                dst[i].a = color[3];
+            }
         }
     }
+}
+
+void draw_string(int x, int y, char *string, unsigned char color[4]) {
+    Vert_Buffer vert_buf = {0};
+    render_string(x, y, string, color, &vert_buf);
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0, vert_buf.vert_count * sizeof(vert_buf.verts[0]), vert_buf.verts);
+    glDrawArrays((GL_TRIANGLES), 0, vert_buf.vert_count);
 }
 
 void append_char_to_line(Text_Buffer *text_buffer, int cursor_line, char c) {
